877409539f9c26e735b198e2836dc022606d5785
José <josevictortf@gmail.com>
2020-11-30 13:55:03 -0300

    Exercício de Variáveis

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,75 @@
+/* INTERPRETAÇÃO
+
+Exercício 1.
+O Código que será impresso na primeira linha do console é 10.
+O Código que será impresso na segunda linha do console vai ser 10 e 5.
+*/
+
+/* Exercício 2:
+ Será impresso no console: 10 10 10.
+*/
+
+// ESCRITA
+
+// EXERCÍCIO 1
+
+// Exercícios A, B e C:
+
+// const nome
+// console.log(typeof nome);
+
+// const idade 
+// console.log(typeof idade);
+
+// O Tipo undefined foi impresso pois não declaramos valor nenhum.
+
+// Exercício D, E e F:
+
+// let nome = prompt ("Qual seu nome?")
+// let idade = prompt ("Qual sua idade?")
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+// console.log (`Olá ${nome}, você tem ${idade} anos.`)  
+
+// O tipo string foi impresso pois declaramos uma pergunta a varíavel.
+
+
+/* EXERCÍCIO 2
+
+const pergunta1 = prompt ("Qual cidade você mora?")
+const pergunta2 = prompt ("Qual o nome do seu cachorro?")
+const pergunta3 = prompt ("Quantos anos você tinha quando fez a 5ª Série?")
+const pergunta4 = prompt ("Qual sua cor favorita?")
+const pergunta5 = prompt ("Desktop ou Notebook?")
+
+console.log("Resposta: " +pergunta1);
+console.log("Resposta: " +pergunta2);
+console.log("Resposta: " +pergunta3);
+console.log("Resposta: " +pergunta4);
+console.log("Resposta: " +pergunta5);
+
+*/
+
+/* EXERCÍCIO 3
+
+let array = ['Strogonoff','Frango Assado','Batata Frita', 'Arroz', 'Feijão']
+console.log('Essas são minhas comidas preferidas: ' +array);
+
+let comida = prompt ("Qual sua comida preferida?")
+array[1] = comida
+console.log(array);
+
+*/
+
+
+/* EXERCÍCIO 4
+
+// let perguntas = ['Você já dormiu hoje?', 'Você já almoçou hoje?', 'Você já correu hoje?']
+// let respostas = [true, false]
+
+// console.log(perguntas[0], respostas[0]);
+// console.log(perguntas[1] , respostas [0]);
+// console.log(perguntas [2] , respostas [1]);
+
+*/
