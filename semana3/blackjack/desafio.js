d531ef9f0f38f17bee1a9cd02e1c501775755859
José <josevictortf@gmail.com>
2020-12-04 16:14:44 -0300

    Exercício completo e desafio incompleto

---
+++ semana3/blackjack/desafio.js
@@ -0,0 +1,42 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+//  console.log("Bem vindo ao jogo de BlackJack");
+
+//  let cartaHuman = [comprarCarta(), comprarCarta()]
+//  let cartaPc = [comprarCarta(), comprarCarta()]
+
+//  let somaHuman = cartaHuman[0].valor + cartaHuman[1].valor 
+//  let somaPc = cartaPc[0].valor + cartaPc[1].valor
+
+//  if(confirm("Quer iniciar uma nova rodada?")) {
+    
+   // while(cartaHuman[0] === 11 && cartaHuman[1] === 11 && cartaPc[0] === 11 && cartaHuman[1] === 11){
+
+   //    let cartaHuman = [comprarCarta(), comprarCarta()]
+   //    let cartaPc = [comprarCarta(), comprarCarta()]
+
+   //    let somaHuman = cartaHuman[0].valor + cartaHuman[1].valor 
+   //    let somaPc = cartaPc[0].valor + cartaPc[1].valor
+   // }
+   // if (confirm(`Suas cartas são ${cartaHuman[0].texto} ${cartaHuman[1].texto}. A carta revelado do computador é ${cartaPc[1].texto}.\n Deseja comprar mais uma carta?`)){
+   // }
+
+   // cartaHuman.push(comprarCarta())
+   // let maisSomaHuman = somaHuman + cartaHuman[cartaHuman.length]
+
+   // if (maisSomaHuman === 21){
+   // }
+   // alert (`Suas cartas são ${cartaHuman[0].texto} ${cartaHuman[1].texto} ${cartaHuman[2].texto}` )
+
+//  }
