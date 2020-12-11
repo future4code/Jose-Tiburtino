10a5447c5c60b3e42311a91cda086b313afb182a
José <josevictortf@gmail.com>
2020-12-11 10:44:57 -0300

    Realizado o exercício de interpretação 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,9 @@
+// Exercícios de interpretação de código:
+
+// Interpretação 1:
+// O código está pedindo para o usuário inserir o valor da cotação do Dólar atual para ser convertido em reais.
+// No return ele está pegando o valor que é inserido no parâmetro da função e multiplicando pela cotação que o usuário inseriu.
+// A função está pegando um valor que vem de uma varíavel na linha 7.
+// O valor impresso será o valor que o usuário inseriu * 100. Ex: (5*100 = 500)
+
+
