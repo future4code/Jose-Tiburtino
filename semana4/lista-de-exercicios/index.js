152ee66e73acc31ad049b9040cd57ca4b254dfa6
José <josevictortf@gmail.com>
2020-12-11 10:55:34 -0300

    Realizado o exercício de interpretação 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,4 +6,10 @@
 // A função está pegando um valor que vem de uma varíavel na linha 7.
 // O valor impresso será o valor que o usuário inseriu * 100. Ex: (5*100 = 500)
 
+// Interpretação 2:
+// A função foi criada para escolher um tipo de investimento e inserir um valor, os tipos de investimentos tem diferentes valores ao final de cada um.
+// Recebe o tipo de investimento (se o investimento escolhido estiver incorreto será dado um alert), o valor inserido e será multiplicado por uma "taxa" fixa que será retornado. 
+// No primeiro console.log será impresso = 150 * 1.1 = 165
+// No segundo console.log não será impresso nada e dará um alert que informa o usuário que o tipo de investimento está incorreto.
+
 
