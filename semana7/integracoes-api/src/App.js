b725b856dff72da3f6b18d99edc4373ffac9e01e
José <josevictortf@gmail.com>
2021-01-12 15:47:37 -0300

    Realizado o exercício e 1º desafio

---
+++ semana7/integracoes-api/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import CreateUser from "./Components/CreateUser/CreateUser";
+import GetUser from "./Components/GetUser/GetUser";
+
+const Container = styled.div``;
+
+const Button = styled.button``;
+
+class App extends React.Component {
+  state = {
+    section: "",
+    id: "",
+  };
+
+  changeSection = (event) => {
+    this.setState({ section: event.target.getAttribute("value") });
+  };
+
+  receiveId = (clickId) => {
+    this.setState({ section: "details", id: clickId });
+  };
+
+  render() {
+    if (this.state.section === "list") {
+      return (
+        <Container>
+          <Button onClick={this.changeSection} value="getUser">
+            Ir para a página de registro
+          </Button>
+          <GetUser getId={this.receiveId} />
+        </Container>
+      );
+    }
+
+    return (
+      <Container>
+        <Button onClick={this.changeSection} value="list">
+          Ir para a lista de Usuários
+        </Button>
+        <CreateUser />
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ semana7/integracoes-api/src/Components/GetUser/GetUser.js
@@ -0,0 +1,98 @@
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const UserContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 35px;
+  box-sizing: border-box;
+`;
+
+const UlUsers = styled.div`
+  list-style-type: none;
+  padding: 0;
+  width: 30%;
+`;
+
+const DivList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid black;
+  margin: 15px;
+`;
+
+const ButtonDelete = styled.button`
+  color: red;
+`;
+
+class GetUser extends React.Component {
+  state = {
+    usersList: [],
+  };
+
+  getUsers = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        {
+          headers: {
+            Authorization: "jose-victor-epps",
+          },
+        }
+      )
+      .then((response) => {
+        this.setState({ usersList: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  deleteItem = (id) => {
+    if (window.confirm("Tem certeza de que deseja deletar?")) {
+      axios
+        .delete(
+          `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
+          {
+            headers: {
+              Authorization: "jose-victor-epps",
+            },
+          }
+        )
+        .then((response) => {
+          return alert("Usuário deletado com sucesso!"), this.getUsers();
+        })
+        .catch((error) => {
+          return alert("Usuário não foi deletado");
+        });
+    }
+  };
+
+  render() {
+    return (
+      <UserContainer>
+        <h2>Usuários Cadastrados:</h2>
+        <UlUsers>
+          {this.state.usersList.map((user) => {
+            return (
+              <DivList>
+                <li onClick={() => this.props.pegaId(user.id)}>{user.name}</li>
+                <ButtonDelete onClick={() => this.deleteItem(user.id)}>
+                  X
+                </ButtonDelete>
+              </DivList>
+            );
+          })}
+        </UlUsers>
+      </UserContainer>
+    );
+  }
+}
+
+export default GetUser;
