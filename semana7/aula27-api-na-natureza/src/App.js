ada674c9a6e48f8230b18bf1a2d2afed8e83274f
José <josevictortf@gmail.com>
2021-01-14 15:49:31 -0300

    Realização do exercício

---
+++ semana7/aula27-api-na-natureza/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Pokedex from "./Components/Pokedex/Pokedex";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Pokedex />
+      </div>
+    );
+  }
+}
+
+export default App;
