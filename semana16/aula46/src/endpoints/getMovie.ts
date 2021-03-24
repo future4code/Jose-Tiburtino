1610da87b8af079d1331729cc0d359083206a96a
José <josevictortf@gmail.com>
2021-03-24 18:02:04 -0300

    Exercícios finalizados

--- semana16/aula46/src/endpoints/getMovie.ts
+++ semana16/aula46/src/endpoints/getMovie.ts
@@ -5,15 +5,38 @@ export const createMovie = async (
   title: string,
   synopsis: string,
   releaseDate: Date,
-  playingLimitDate: Date
+  playingLimitDate: Date,
+  rating: number
 ) => {
   await connection
     .insert({
       id: id,
       title: title,
       synopsis: synopsis,
-      releas_date: releaseDate,
+      release_Date: releaseDate,
       playing_limit_date: playingLimitDate,
+      rating: rating,
     })
     .into("Movie");
+};
+
+export const getMovies = async (): Promise<void> => {
+  const result = await connection.raw(`
+        SELECT * FROM Movie
+    `);
+  return result[0];
+};
+
+export const searchMovies = async (
+  title: string
+  //   synopsis: string
+): Promise<any> => {
+  try {
+    const result = await connection.raw(`
+        SELECT * FROM Movie WHERE title OR synopsis LIKE "%${title}%"
+    `);
+    return result[0];
+  } catch (error) {
+    return [];
+  }
 };
--- semana16/aula46/src/index.ts
+++ semana16/aula46/src/index.ts
@@ -5,6 +5,7 @@ import {
   getActorByName,
   countActors,
 } from "./endpoints/getActor";
+import { createMovie, getMovies, searchMovies } from "./endpoints/getMovie";
 import {
   updateSalary,
   deleteActor,
@@ -77,3 +78,41 @@ app.get(
     }
   }
 );
+
+app.post("/movie/create", async (req: Request, res: Response) => {
+  let errorCode: number = 400;
+  try {
+    await createMovie(
+      req.body.id,
+      req.body.title,
+      req.body.synopsis,
+      req.body.releaseDate,
+      req.body.playingLimitDate,
+      req.body.rating
+    );
+    res.status(200).send("Filme criado!");
+  } catch (error) {
+    res.status(errorCode).send(error.message);
+  }
+});
+
+app.get("/movie/all", async (req: Request, res: Response) => {
+  let errorCode: number = 400;
+  try {
+    const result = await getMovies();
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(errorCode).send({ message: error.message });
+  }
+});
+
+app.get("/movie/search", async (req: Request, res: Response) => {
+  let errorCode: number = 400;
+  const query: string = req.query.query as string;
+  try {
+    const result = await searchMovies(query);
+    res.status(200).send({ movies: result });
+  } catch (error) {
+    res.status(errorCode).send({ message: error.message });
+  }
+});
