dc94c3062c07785ee1d8978af35e61b671b2ddbb
José Victor T. Ferreira <josevictortf@gmail.com>
2021-04-14 13:43:12 -0300

    Tables created

--- semana19/projeto-labook/src/database/migrations/migrations.ts
+++ semana19/projeto-labook/src/database/migrations/migrations.ts
@@ -3,7 +3,24 @@ import { Connection } from "../connection";
 class Migrations extends Connection {
   public createTables = async (): Promise<void> => {
     try {
-      await Connection.connection.raw(``);
+      await Connection.connection.raw(`
+      CREATE TABLE IF NOT EXISTS Labook_Users(
+        id VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+     );
+
+     CREATE TABLE IF NOT EXISTS Labook_Posts(
+        id VARCHAR(255) PRIMARY KEY,
+        photo VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        type ENUM("normal","event") DEFAULT "normal",
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        author_id VARCHAR(255),
+        FOREIGN KEY (author_id) REFERENCES Labook_Users(id)
+     )
+    `);
 
       console.log("Tables created.");
 
@@ -15,4 +32,4 @@ class Migrations extends Connection {
   };
 }
 
-export { Migrations };
+new Migrations().createTables();
