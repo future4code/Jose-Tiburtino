90ee2ace2b7a1e1dca4e8ada34389a64756ea5b2
José Victor T. Ferreira <josevictortf@gmail.com>
2021-04-20 12:38:21 -0300

    Start

---
+++ semana20/aula59/tests/validateCharacter.test.ts
@@ -0,0 +1,14 @@
+import { validateCharacter } from "../src/validateCharacter";
+
+describe("Validate characters", () => {
+  test("Should return false for empty name", () => {
+    const result = validateCharacter({
+      name: "",
+      life: 1500,
+      strength: 300,
+      defense: 500,
+    });
+
+    expect(result).toBe(false);
+  });
+});
